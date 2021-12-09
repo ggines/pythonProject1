827b7a09a07fbb7e6eb8317321b53d7751c19666
ggines <ggines@institutmvm.cat>
2021-12-09 13:01:12 +0100

    Initial commit

--- A11-Ex4.py
+++ A11-Ex4.py
@@ -1,7 +1,7 @@
 def main():
     state= "Practica els problemes de list comprehensions per a ser més Pythonic!"
     espacios= [x for x in state if x in " "]
-    print(len(espacios))
+    print("Contiene",len(espacios),"espacios")
 
 if __name__ == '__main__':
     main()
