15d423cb0abbab08d1944e5973ea296efb558ff2
ggines <95416761+ggines@users.noreply.github.com>
2022-02-24 12:46:35 +0100

    Add files via upload

---
+++ Ex1/main.py
@@ -0,0 +1,10 @@
+#demana nom del fitxer, que vol introduïr i ho mostri
+#from functions import file_open
+from functions import file_write
+#from functions import check_file
+MSG= "Introdueix el nombre del fitxer: "
+
+def main():
+    file_write('text.txt')
+if __name__ == '__main__':
+    main()
