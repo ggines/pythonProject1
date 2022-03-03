9e2096c9a4c55a1d51fcd57b5ce7507d5352a045
ggines <95416761+ggines@users.noreply.github.com>
2022-03-03 19:23:13 +0100

    Update main.py

--- Ex6/main.py
+++ Ex6/main.py
@@ -4,6 +4,7 @@ from functions import file_write
 #from functions import check_file
 MSG= "Vols introduir més registres? Sí=1 No=0 "
 MSG_Error= "Error"
+
 def main():
     file_header('files/books.csv')
     opcion=1
