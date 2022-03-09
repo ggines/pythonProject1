b05a19e2cf4521a4c9a1d6ea0cc7134c189e7519
ggines <95416761+ggines@users.noreply.github.com>
2022-03-09 17:11:06 +0100

    Add files via upload

---
+++ Ex9/functions.py
@@ -0,0 +1,26 @@
+import csv
+
+def file_check(fname):
+  try:
+    f = open(fname, "r")
+    return True
+  except FileNotFoundError:
+    return False
+
+def file_header(fname):
+  with open(fname, 'a', encoding='utf-8', newline='') as csv_file:
+    fieldnames = ['ID', 'Grup', 'Nom', 'Data', 'Visites']
+    writeCSV = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writeCSV.writeheader()
+
+def file_write(fname, video, ID, data):
+  with open(fname, 'a', encoding='utf-8', newline='') as csvfile:
+    fieldnames = ['ID', 'Grup', 'Nom', 'Data', 'Visites']
+    writeCSV = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writeCSV.writerow({'ID': ID, 'Grup': video['grup'], 'Nom': video['nom'], 'Data': data, 'Visites': video['views']})
+
+def file_read(fname):
+  with open(fname) as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+      print(' '.join(row))
---
+++ Ex9/main.py
@@ -0,0 +1,34 @@
+import csv
+from functions import file_check
+from functions import file_header
+from functions import file_write
+from functions import file_read
+MSG= "Quants vídeos vols introduir? "
+MSG_grup= "Introdueix el grup/cantant del vídeo: "
+MSG_nom= "Introdueix el nom del vídeo: "
+MSG_dia= "Introdueix el día de publicació: "
+MSG_mes= "Introdueix el mes de publicació: "
+MSG_year= "Introdueix l'any de publicació: "
+MSG_views= "Introdueix les visites del vídeo: "
+MSG_Error= "Error"
+
+def main():
+    count= int(input(MSG))
+    check= file_check('videos.csv')
+    if check == False:
+        file_header('videos.csv')
+    ID = 0
+    for x in range(count):
+        video= dict()
+        video['grup'] = input(MSG_grup)
+        video['nom'] = input(MSG_nom)
+        video['dia'] = input(MSG_dia)
+        video['mes'] = input(MSG_mes)
+        video['year'] = input(MSG_year)
+        video['views'] = input(MSG_views)
+        ID += 1
+        data= video['dia'] + '/' + video['mes'] + '/' + video['year']
+        file_write('videos.csv', video, ID, data)
+    file_read('videos.csv')
+if __name__ == '__main__':
+    main()
